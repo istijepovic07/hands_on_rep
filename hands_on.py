06dddf45c426e62a0bf3edbecf8e9922fef8a8d4
istijepovic07 <113998510+istijepovic07@users.noreply.github.com>
2022-09-20 15:55:03 +0200

    Add files via upload

---
+++ hands_on.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 20 14:48:57 2022
+
+@author: Korisnik
+"""
+
+import streamlit as st
+
+st.title("Ivana Hello")
+
+my_text = st.text("Random text")
+my_button = st.button("Try")
